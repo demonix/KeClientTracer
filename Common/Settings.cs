79b17c3c9021249f695bbd2e26d346c3f948082c
demonix <demonixed@gmail.com>
2011-01-10 01:08:38 +0500

    settings

---
+++ Common/Settings.cs
@@ -0,0 +1,93 @@
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+
+namespace Common
+{
+    public class Settings
+    {
+        private static object _instanceLocker = new object();
+        private string _fileName;
+        private string _separator;
+        private static Settings _instance;
+
+
+        public string TryGetValue(string key)
+        {
+            if (!_settings.ContainsKey(key)) return null;
+            List<string> values = _settings[key];
+            return values[values.Count - 1];
+        }
+
+        private Settings(string fileName,string separator, Encoding encoding)
+        {
+           _fileName = fileName;
+			_separator = separator;
+			if (!File.Exists(fileName))
+				return;
+            using (FileStream fileStream = new FileStream(fileName, FileMode.Open, FileAccess.Read, FileShare.ReadWrite))
+                Init(fileStream, encoding);
+        }
+
+        public static Settings GetInstance()
+        {
+            lock (_instanceLocker)
+            {
+                if (_instance == null)
+                {
+                    _instance = new Settings("settings\\settings",":=",Encoding.Default);
+                }
+            }
+            return _instance;
+        }
+
+
+
+
+        private Settings(Stream stream, Encoding encoding, string separator)
+		{
+			this._separator = separator;
+			Init(stream, encoding);
+		}
+
+		private void Init(Stream stream, Encoding encoding)
+		{
+			using (StreamReader reader = new StreamReader(stream, encoding))
+			{
+				while (!reader.EndOfStream)
+				{
+					string line = reader.ReadLine();
+					if (IsEmpty(line)) continue;
+					KeyValuePair<string, string> keyValuePair = GetKeyAndValue(line);
+					if (!_settings.ContainsKey(keyValuePair.Key))
+						_settings.Add(keyValuePair.Key, new List<string>());
+					_settings[keyValuePair.Key].Add(keyValuePair.Value);
+				}
+			}
+		}
+
+        private KeyValuePair<string, string> GetKeyAndValue(string line)
+        {
+            CheckLineFormat(line);
+            int i = line.IndexOf(_separator);
+            string key = line.Substring(0, i).Trim();
+            string value = line.Substring(i + _separator.Length, line.Length - i - _separator.Length).Trim();
+            return new KeyValuePair<string, string>(key, value);
+        }
+
+        private void CheckLineFormat(string line)
+        {
+            if (line.IndexOf(_separator) == -1)
+                throw new Exception(string.Format("Can't find separator '{0}' in config line '{1}'.", _separator, line));
+        }
+
+
+        private static bool IsEmpty(string line)
+        {
+            string t = line.TrimStart(' ', '\t');
+            return (string.IsNullOrEmpty(t)) || t.StartsWith("#");
+        }
+        private readonly Dictionary<string, List<string>> _settings = new Dictionary<string, List<string>>();
+    }
+}
